5115bc24eface9c684befa91b0252cc4a4436b55
Matthew Pomes <matthew.pomes@pm.me>
2023-02-06 13:05:44 -0600

    Add sqlite3

--- pages/api/major.ts
+++ pages/api/major.ts
@@ -1,21 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import sql from "../../services/sql";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-): void {
-  res.status(200).json([
-    {
-      idMajor: 2,
-      name: "Computer Science"
-    },
-    {
-      idMajor: 4,
-      name: "Applied Math and Computer Science"
-    },
-    {
-      idMajor: 5,
-      name: "Psychology"
-    }
-  ]);
+): Promise<void> {
+  const con = await sql();
+  const rows = await con.all("SELECT * FROM Majors");
+  res.status(200).json(rows);
+  // res.status(200).json([
+  //   {
+  //     idMajor: 2,
+  //     name: "Computer Science"
+  //   },
+  //   {
+  //     idMajor: 4,
+  //     name: "Applied Math and Computer Science"
+  //   },
+  //   {
+  //     idMajor: 5,
+  //     name: "Psychology"
+  //   }
+  // ]);
 }

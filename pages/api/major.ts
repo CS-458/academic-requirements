8caba1963d5135b90739d059b627074d1e5dd30c
Matthew Pomes <matthew.pomes@pm.me>
2023-02-06 13:56:55 -0600

    Import data to sqlite db

--- pages/api/major.ts
+++ pages/api/major.ts
@@ -6,20 +6,6 @@ export default async function handler(
   res: NextApiResponse
 ): Promise<void> {
   const con = await sql();
-  const rows = await con.all("SELECT * FROM Majors");
+  const rows = await con.all("SELECT * FROM major");
   res.status(200).json(rows);
-  // res.status(200).json([
-  //   {
-  //     idMajor: 2,
-  //     name: "Computer Science"
-  //   },
-  //   {
-  //     idMajor: 4,
-  //     name: "Applied Math and Computer Science"
-  //   },
-  //   {
-  //     idMajor: 5,
-  //     name: "Psychology"
-  //   }
-  // ]);
 }

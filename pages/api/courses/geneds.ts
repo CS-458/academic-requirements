6530e6a81b2f90cbda58a61777d5deb00ec2186e
RaffelNicholas <raffeln6264@my.uwstout.edu>
2023-02-08 14:12:55 -0600

    Fixed API calls

--- pages/api/courses/geneds.ts
+++ pages/api/courses/geneds.ts
@@ -1,21 +1,34 @@
+/*
+    @Description
+      This is the API call that gets the all the GenED courses
+*/
+
 import { NextApiRequest, NextApiResponse } from "next";
+import { Course } from "../../../entities/four_year_plan";
+import sql from "../../../services/sql";
 
-const majorIDs: {
-  [key: string]: number;
-} = {
-  "Computer Science": 2
-};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-): void {
-  if (typeof req.query.mname === "string") {
-    const majorId = majorIDs[req.query.mname];
-    if (majorId !== undefined) {
-      res.status(200).json([{ majorId }]);
-      return;
-    }
+): Promise<void> {
+  if(typeof req.query.conid === "string"){
+    //Creates connection to the DB
+    const con = await sql();
+
+    //queries the DB for all GenEDs and saves it into the rows var
+    const rows = await con.all(
+      ` SELECT co.subject, co.number, co.credits, co.semesters, co.name, co.preReq, co.idCourse, cat.name AS 'category', cat.idCategory
+        FROM category cat
+        JOIN coursecategory cc ON cc.categoryId = cat.idCategory
+        JOIN course co ON co.idCourse = cc.courseId
+        WHERE cat.idCategory NOT IN (SELECT categoryId FROM majorCategory) AND 
+        cat.idCategory NOT IN (SELECT categoryId FROM concentrationCategory)
+        ORDER BY co.subject, co.number `
+    );
+    //Returns the data queried from the DB onto the screen
+    res.status(200).json(rows);
+    return;
   }
   res.status(400).json({ error: "Major not defined" });
 }
--- pages/api/requirements/gen.ts
+++ pages/api/requirements/gen.ts
@@ -1,21 +1,29 @@
+/*
+    @Description
+      This is the API call that gets all the GenEds for a given Concentration
+*/
+
 import { NextApiRequest, NextApiResponse } from "next";
+import { Course } from "../../../entities/four_year_plan";
+import sql from "../../../services/sql";
 
-const majorIDs: {
-  [key: string]: number;
-} = {
-  "Computer Science": 2
-};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-): void {
-  if (typeof req.query.mname === "string") {
-    const majorId = majorIDs[req.query.mname];
-    if (majorId !== undefined) {
-      res.status(200).json([{ majorId }]);
-      return;
-    }
-  }
-  res.status(400).json({ error: "Major not defined" });
+): Promise<void> {
+  //Creates connection to the DB
+  const con = await sql();
+
+  //queries the DB for all the GenEds for a given Concentration
+  const rows = await con.all(
+    ` SELECT c.idCategory, c.name, c.parentCategory, cr.creditCount, cr.courseCount, cr.courseReqs
+      FROM category c
+      JOIN categoryrequirements cr ON cr.categoryId = c.idCategory
+      WHERE c.idCategory NOT IN (SELECT categoryId FROM majorCategory) AND c.idCategory NOT IN (SELECT categoryId FROM concentrationCategory) `
+  );
+  //Returns the data queried from the DB onto the screen
+  res.status(200).json(rows);
+  return;
+  res.status(400).json({ error: "Concentration not defined" });
 }

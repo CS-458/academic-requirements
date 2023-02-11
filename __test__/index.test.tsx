d5c71a2904ede69319f959471b9d828b2e90ec35
Matthew Pomes <matthew.pomes@pm.me>
2023-02-11 17:51:39 -0600

    Fully working API routes in testing

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,29 +1,22 @@
 import "@testing-library/jest-dom";
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 import Home from "../pages/index";
-import { wrapper } from "./util";
+import { setupUser, wrapper } from "./util";
 
 test("Verify Majors and Concentrations", async () => {
-  const user = userEvent.setup();
+  const user = setupUser();
   const index = render(wrapper(<Home />));
   expect(index.baseElement).toMatchSnapshot();
 
   const generateButton = screen.getByText(/Generate Schedule/i);
   expect(generateButton).toBeDisabled();
 
-  await waitFor(async () => {
-    await user.click(screen.getByLabelText(/Major/i));
-    await user.click(screen.getByText(/Computer Science/i));
-  });
+  await user.selectAutocomplete(/Major/i, /^Computer Science$/i);
   expect(index.baseElement).toMatchSnapshot();
   expect(generateButton).toBeDisabled();
 
-  await waitFor(async () => {
-    await user.click(screen.getByLabelText(/Concentration/i));
-    await user.click(screen.getByText(/Mobile Applications/i));
-  });
+  await user.selectAutocomplete(/Concentration/i, /Mobile Applications/i);
   expect(index.baseElement).toMatchSnapshot();
   expect(generateButton).not.toBeDisabled();
 });
--- __test__/util.tsx
+++ __test__/util.tsx
@@ -1,20 +1,49 @@
+import { jest } from "@jest/globals";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { testApiHandler } from "next-test-api-route-handler";
 import { NextApiHandler } from "next";
+import { Matcher, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
+
+interface UserExt {
+  /// Select an element from an `AutoComplete` dropdown
+  selectAutocomplete: (label: Matcher, option: Matcher) => Promise<void>;
+}
+
+export function setupUser(): UserEvent & UserExt {
+  return {
+    ...userEvent.setup(),
+    selectAutocomplete: async function (
+      this: UserEvent,
+      label: Matcher,
+      option: Matcher
+    ) {
+      await this.click(
+        await screen.findByLabelText(label, { selector: "input" })
+      );
+      await this.click(await screen.findByText(option));
+    }
+  };
+}
 
 export async function fetchReplace(initial: string): Promise<Response> {
+  if (jest.isEnvironmentTornDown()) {
+    throw new Error("Environment has been torn down");
+  }
   const res = { resolve: (_a: any) => {}, reject: (_e: any) => {} };
   const result: Promise<Response> = new Promise((resolve, reject) => {
     res.resolve = resolve;
     res.reject = reject;
   });
   const [url, params] = initial.split("?");
+  const handler: NextApiHandler = await import(`../pages/${url}`);
   await testApiHandler({
-    handler: await import(`../pages/${url}`),
+    handler,
     url: initial,
-    params: params.split("&").reduce((a, b) => {
+    params: params?.split("&").reduce((a, b) => {
       const [name, value] = b.split("=");
       return { ...a, [name]: value };
     }, {}),
@@ -36,8 +65,10 @@ export function wrapper(children: JSX.Element | JSX.Element[]): JSX.Element {
   window.fetch = fetchReplace;
   // <DndProvider backend={HTML5Backend}>{children}</DndProvider>
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      {children}
-    </QueryClientProvider>
+    <div data-testid="test-root-element">
+      <QueryClientProvider client={new QueryClient()}>
+        {children}
+      </QueryClientProvider>
+    </div>
   );
 }

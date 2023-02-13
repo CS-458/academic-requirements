3ffb29bc40e667cbb945f79d7d58a848dc2e9b7c
MrZalgo <98833844+MrZalgo@users.noreply.github.com>
2023-02-13 14:41:10 -0600

    Google authentication start
    Attempts to call into Google Client ID API. Running into issue that results in it not recognizing google as an ID.

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -5,11 +5,32 @@ import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
+import { GOOGLE_FONT_PROVIDER } from "next/dist/shared/lib/constants";
+import { useEffect } from "react";
+import { GoogleAuth } from "google-auth-library";
+
 export default function DefaultLayout(props: {
   children: JSX.Element | JSX.Element[];
 }): JSX.Element {
+function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+
+  useEffect(() => {
+    google.account.id.initialize({
+      client_id: "903868134209-gci8nov2ch84ge895l8sd3toore5bvtb.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+  }, []);
+
   return <div>
     <Box sx={{ flexGrow: 1 }}>
+    <script src = "https://accounts.google.com/gsi/client" async defer></script>
   <AppBar position="static">
     <Toolbar>
     <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>

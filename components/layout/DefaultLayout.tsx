619de0d7eb19260d69ea59a013a2493a7f4b2de9
Matthew Pomes <matthew.pomes@pm.me>
2023-02-15 11:14:46 -0600

    Working Google Authentication

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -2,46 +2,81 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
-import { GOOGLE_FONT_PROVIDER } from "next/dist/shared/lib/constants";
-import { useEffect } from "react";
-import { GoogleAuth } from "google-auth-library";
+// import { GOOGLE_FONT_PROVIDER } from "next/dist/shared/lib/constants";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { useState } from "react";
+import jwtDecode from "jwt-decode";
+
+interface UserInfo {
+  /// Profile URL
+  picture: string;
+  /// User ID
+  sub: string;
+  name: string;
+  email: string;
+}
+
+interface User {
+  info: UserInfo;
+  cred: string;
+}
 
 export default function DefaultLayout(props: {
   children: JSX.Element | JSX.Element[];
 }): JSX.Element {
-function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
+  const [user, setUser] = useState<User | undefined>(undefined);
+
+  function responseMessage(token: CredentialResponse) {
+    console.log(token);
+    if (token.credential !== undefined) {
+      const jwt: UserInfo = jwtDecode(token.credential);
+      console.log(jwt);
+      setUser({ info: jwt, cred: token.credential });
+      fetch("/api/login", {
+        method: "POST",
+        body: JSON.stringify({
+          token: token.credential
+        })
+      }).then(console.log, console.error);
+    }
+  }
+  function errorMessage() {
+    console.error("Login failed");
   }
 
-  useEffect(() => {
-    google.account.id.initialize({
-      client_id: "903868134209-gci8nov2ch84ge895l8sd3toore5bvtb.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    });
-
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large" }
-    );
-  }, []);
-
-  return <div>
-    <Box sx={{ flexGrow: 1 }}>
-    <script src = "https://accounts.google.com/gsi/client" async defer></script>
-  <AppBar position="static">
-    <Toolbar>
-    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-      <MenuIcon />
-    </IconButton>
-    <img src="/logo-new.svg" height="60" alt="logo" />
-    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}/>
-    <Button color="inherit">Login</Button>
-    </Toolbar>
-  </AppBar>
-</Box>
-{props.children}
-</div>;
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <img src="/logo-new.svg" height="60" alt="logo" />
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} />
+            {user === undefined ? (
+              <GoogleLogin
+                onSuccess={responseMessage}
+                onError={errorMessage}
+                useOneTap
+              />
+            ) : (
+              <Typography variant="h5" component="div">
+                <span>{user.info.name}</span>
+                <img src={user.info.picture} style={{ height: "1em" }} />
+              </Typography>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {props.children}
+    </div>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,24 +8,28 @@ import type { AppProps } from "next/app";
 import DefaultLayout from "../components/layout/DefaultLayout";
 import React from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import secrets from "../secrets.json";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate
-          state={
-            // @ts-expect-error
-            pageProps.dehydratedState
-          }
-        >
-          <DefaultLayout>
-            <Component {...pageProps} />
-          </DefaultLayout>
-        </Hydrate>
-      </QueryClientProvider>
+      <GoogleOAuthProvider clientId={secrets.client.id}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate
+            state={
+              // @ts-expect-error
+              pageProps.dehydratedState
+            }
+          >
+            <DefaultLayout>
+              <Component {...pageProps} />
+            </DefaultLayout>
+          </Hydrate>
+        </QueryClientProvider>
+      </GoogleOAuthProvider>
     </>
   );
 }

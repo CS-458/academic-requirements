5377d18e5db447092afce202c253a5fec6f0f33d
ruudkas <kassidyruud@gmail.com>
2023-02-22 15:15:32 -0600

    remove todos

--- components/FourYearPlanPage.tsx
+++ components/FourYearPlanPage.tsx
@@ -447,7 +447,7 @@ export const FourYearPlanPage: FC<FourYearPlanType> = memo(
         //  Check if the course is offered in the semester it was dragged to
         if (checkCourseSemester(updateWarning.course, updateWarning.newSemester)) {
           //  If the course is not offered during the semester, add it to the warning course list
-          if (warningFallvsSpringCourses.find((x) => x === updateWarning.course) === undefined) { // TODO
+          if (warningFallvsSpringCourses.find((x) => x === updateWarning.course) === undefined) {
             warningFallvsSpringCourses.push(updateWarning.course);
             setVisibility(true);
             setErrorMessage(
@@ -658,7 +658,7 @@ export const FourYearPlanPage: FC<FourYearPlanType> = memo(
       // Remove any courses that were marked as warning, but now have resolved prerequisites
       if (!movedRight) {
         warningPrerequisiteCourses.forEach((currentWarningCourse) => {
-          if (initialPreviousCourses.find((prevCourse) => prevCourse === currentWarningCourse) === undefined) { // TODO
+          if (initialPreviousCourses.find((prevCourse) => prevCourse === currentWarningCourse) === undefined) {
             failedCoursesList.forEach((currentFailedCourse) => {
               if (currentWarningCourse === currentFailedCourse) {
                 found = true;
@@ -690,13 +690,13 @@ export const FourYearPlanPage: FC<FourYearPlanType> = memo(
         let message = "";
         //  Push each failed course to the warningCourses
         failedCoursesList.forEach((x) => {
-          if (warningPrerequisiteCourses.find((z) => z === x) === undefined) { // TODO
+          if (warningPrerequisiteCourses.find((z) => z === x) === undefined) {
             const temp = warningPrerequisiteCourses;
             temp.push(x);
             setWarningPrerequisiteCourses(temp);
           }
           //  If the course is failing, but not due to the latest course move, modify the warning message
-          if (failedCoursesNoWarning.find((z) => z === x) === undefined) { // TODO
+          if (failedCoursesNoWarning.find((z) => z === x) === undefined) {
             message.length > 0
               ? (message = message + "," + x.subject + "-" + x.number)
               : (message = message + x.subject + "-" + x.number);

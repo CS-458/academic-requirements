0d10f1b565fdf4dcf74d3f0fe995be04346a3fa8
ruudkas <kassidyruud@gmail.com>
2023-02-27 14:35:29 -0600

    Create google authentication

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -2,26 +2,82 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
-import LogoLink from "./LogoLink";
+// import { GOOGLE_FONT_PROVIDER } from "next/dist/shared/lib/constants";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { useState } from "react";
+import jwtDecode from "jwt-decode";
+
+interface UserInfo {
+  /// Profile URL
+  picture: string;
+  /// User ID
+  sub: string;
+  name: string;
+  email: string;
+}
+
+interface User {
+  info: UserInfo;
+  cred: string;
+}
 export default function DefaultLayout(props: {
   children: JSX.Element | JSX.Element[];
 }): JSX.Element {
-  return <div>
-    <Box sx={{ flexGrow: 1 }}>
-  <AppBar position="static">
-    <Toolbar>
-    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-      <MenuIcon />
-    </IconButton>
-   <LogoLink/>
-    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}/>
-    <Button color="inherit">Login</Button>
-    </Toolbar>
-  </AppBar>
-</Box>
-{props.children}
-</div>;
+  const [user, setUser] = useState<User | undefined>(undefined);
+
+  function responseMessage(token: CredentialResponse): void {
+    console.log(token);
+    if (token.credential !== undefined) {
+      const jwt: UserInfo = jwtDecode(token.credential);
+      console.log(jwt);
+      setUser({ info: jwt, cred: token.credential });
+      fetch("/api/login", {
+        method: "POST",
+        body: JSON.stringify({
+          token: token.credential
+        })
+      }).then(console.log, console.error);
+    }
+  }
+  function errorMessage(): void {
+    console.error("Login failed");
+  }
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <img src="/logo-new.svg" height="50" alt="logo" />
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} />
+            {user === undefined ? (
+              <GoogleLogin
+                onSuccess={responseMessage}
+                onError={errorMessage}
+                useOneTap
+              />
+            ) : (
+              <Typography variant="h5" component="div">
+                <Box sx={{ m: 0 }}>
+                  <span style={{ margin: 0, position: "absolute", top: "50%", transform: "translateY(-50%)", right: "5rem" }}>{user.info.name}</span>
+                  <img src={user.info.picture} style={{ height: "2em", borderRadius: "50%" }} />
+                </Box>
+              </Typography>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {props.children}
+    </div>
+  );
 }
---
+++ documentation/googleAuthentication.testing.md
@@ -0,0 +1,16 @@
+# Testing for the Google Authentication
+
+## Steps for testing login button
+1. Navigate to any page in the app
+2. Sign in button should appear in upper right inside taskbar on each page
+3. Click on the button
+4. A window should show up prompting the user to login to a google account
+5. User should be able to log in
+    a. click on a currently logged in profile
+    b. click on use another account
+    c. user should be logged in
+6. The sign in button should be replaced with the users name and profile image
+
+## After Sign In
+Navigate to another page to confirm that the user remains logged in
+Testing of our use of the credentials given by google will be automated.

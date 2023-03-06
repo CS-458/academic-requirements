13ebff6893e325f1041c9d684ce53d99838f624c
ruudkas <kassidyruud@gmail.com>
2023-03-06 12:33:40 -0600

    remove log

--- components/DraggableCourse.tsx
+++ components/DraggableCourse.tsx
@@ -57,7 +57,7 @@ export const Course: FC<DragCourseType> = memo(function Course({
       "&filter%5Bcourse_type%5D=-1&filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=21&expand=&navoid=544&search_database=Filter#acalog_template_course_filter";
     return URL;
   }
-  console.log("red", warningRedColor);
+
   return (
     <div
       ref={drag}

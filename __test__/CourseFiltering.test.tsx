4052f3aa8535db965378673dc3bd8250f6512f21
chrisvasterling <chrisvasterling@gmail.com>
2023-03-23 19:36:53 -0500

    CourseFiltering 100% Test Coverage

--- __test__/CourseFiltering.test.tsx
+++ __test__/CourseFiltering.test.tsx
@@ -8,7 +8,7 @@ import { setupUser, render } from "./util";
 const testCourseData: CourseType[] = [
   {
     subject: "TEST-SUB-1",
-    number: "100",
+    number: "100HON",
     name: "Course 1",
     semesters: "",
     credits: 1,
@@ -41,7 +41,7 @@ const testCourseData: CourseType[] = [
     preReq: "",
     idCourse: 295,
     repeatableForCred: false,
-    category: "TEST-CATEGORY-2",
+    category: "TEST-CATEGORY-3",
     idCategory: 20,
     dragSource: ""
   },
@@ -125,20 +125,30 @@ test("Clicking Tabs Change Tab Panel", async () => {
   expect(creditPanel.childNodes.length).toBeGreaterThan(0);
 });
 
-test("Category Information is Correct", async () => {
+test("Category Filtering", async () => {
   const user = setupUser();
   const onFilteredMock = jest.fn();
 
   const index = render(<CourseFiltering courseData={testCourseData} onFiltered={onFilteredMock}/>);
   expect(index.baseElement).toMatchSnapshot();
 
-  await user.click(screen.getByLabelText("Course Category"));
-  testCourseData.forEach((item) => {
-    expect(screen.getByText(item.category)).toBeTruthy();
-  });
+  const categoryTab = screen.getByTestId("category-tab");
+  await user.click(categoryTab);
+
+  for (const course of testCourseData) {
+    await user.selectAutocomplete("Course Category", course.category);
+    expect(onFilteredMock).toHaveBeenCalled();
+    // called with the list of courses for the given course's category without duplicates
+    const expectedOutput = testCourseData
+      .filter(c => c.category === course.category)
+      .filter((c, index, arr) => {
+        return index === arr.findIndex(c2 => c.idCourse === c2.idCourse);
+      });
+    expect(onFilteredMock).toHaveBeenCalledWith(expectedOutput);
+  }
 });
 
-test("Subject/Number Information is Correct", async () => {
+test("Subject/Number - Subject filtering", async () => {
   const user = setupUser();
   const onFilteredMock = jest.fn();
 
@@ -152,34 +162,122 @@ test("Subject/Number Information is Correct", async () => {
   for (const course of testCourseData) {
     await user.selectAutocomplete("Course Subject", course.subject);
     expect(onFilteredMock).toHaveBeenCalled();
-    // expect(onFilteredMock).toHaveBeenCalledWith(
-    //   Array.from(new Set(testCourseData.filter((c) => {
-    //     return c.subject === course.subject;
-    //   })))
-    // );
+    // called with the list of courses for the given course's subject without duplicates
+    const expectedOutput = testCourseData.filter(c => c.subject === course.subject)
+      .filter((c, index, arr) => {
+        return index === arr.findIndex(c2 => c.idCourse === c2.idCourse);
+      });
+    expect(onFilteredMock).toHaveBeenCalledWith(expectedOutput);
   }
-
-  // await user.click(screen.getByLabelText("Course Subject"));
-  // expect(index.baseElement).toMatchSnapshot();
-
-  // testCourseData.forEach((item) => {
-  //   expect(screen.getByText(item.subject)).toBeTruthy();
-  // });
-
-  // await user.selectAutocomplete(/Major/i, /^Psychology$/i);
-
-  // await user.click(screen.getByLabelText("Course Number"));
-  // expect(index.baseElement).toMatchSnapshot();
-
-  // testCourseData.forEach((item) => {
-  //   expect(screen.getByText(item.number)).toBeTruthy();
-  // });
 });
 
-test("Name Information is Correct", () => {
-  expect(true);
+test("Subject/Number - Number filtering", async () => {
+  const user = setupUser();
+  const onFilteredMock = jest.fn();
+
+  const index = render(<CourseFiltering courseData={testCourseData} onFiltered={onFilteredMock}/>);
+  expect(index.baseElement).toMatchSnapshot();
+
+  // Select the Subject/Number filtering option
+  const subjectNumberTab = screen.getByTestId("subjectnumber-tab");
+  await user.click(subjectNumberTab);
+  const numberTextField = screen.getByLabelText("Course Number");
+
+  for (const course of testCourseData) {
+    // await user.selectAutocomplete("Course Number", course.number);
+    await user.type(numberTextField, course.number);
+    expect(onFilteredMock).toHaveBeenCalled();
+    // called with the list of courses for the given course's number without duplicates
+    const expectedOutput = testCourseData
+      .filter(c => c.number.includes(course.number))
+      .filter((c, index, arr) => {
+        return index === arr.findIndex(c2 => c.idCourse === c2.idCourse);
+      });
+    expect(onFilteredMock).toHaveBeenCalledWith(expectedOutput);
+    await user.clear(numberTextField);
+  }
+}, 1000000);
+
+test("Subject/Number - Subject & Number", async () => {
+  const user = setupUser();
+  const onFilteredMock = jest.fn();
+
+  const index = render(<CourseFiltering courseData={testCourseData} onFiltered={onFilteredMock}/>);
+  expect(index.baseElement).toMatchSnapshot();
+
+  const subjectNumberTab = screen.getByTestId("subjectnumber-tab");
+  await user.click(subjectNumberTab);
+  const numberTextField = screen.getByLabelText("Course Number");
+
+  for (const courseForSubject of testCourseData) {
+    await user.selectAutocomplete("Course Subject", courseForSubject.subject);
+
+    for (const courseForNumber of testCourseData) {
+      await user.type(numberTextField, courseForNumber.number);
+
+      const expectedOutput = testCourseData
+        .filter(c => {
+          return (
+            c.subject === courseForSubject.subject &&
+            c.number.includes(courseForNumber.number)
+          );
+        })
+        .filter((c, index, arr) => {
+          return index === arr.findIndex(c2 => c.idCourse === c2.idCourse);
+        });
+
+      expect(onFilteredMock).toHaveBeenCalledWith(expectedOutput);
+      await user.clear(numberTextField);
+    }
+  }
+}, 1000000);
+
+test("Name Filtering", async () => {
+  const user = setupUser();
+  const onFilteredMock = jest.fn();
+
+  const index = render(<CourseFiltering courseData={testCourseData} onFiltered={onFilteredMock}/>);
+  expect(index.baseElement).toMatchSnapshot();
+
+  // Select the Name filtering option
+  const nameTab = screen.getByTestId("name-tab");
+  await user.click(nameTab);
+  const nameTextField = screen.getByLabelText("Course Name");
+
+  await user.type(nameTextField, "a{Backspace}");
+  expect(onFilteredMock).toHaveBeenCalledWith([]);
+
+  for (const course of testCourseData) {
+    await user.type(nameTextField, course.name);
+    // called with the list of courses for the given course's name without duplicates
+    const expectedOutput = testCourseData
+      .filter(c => c.name.toLowerCase().includes(course.name.toLowerCase()))
+      .filter((c, index, arr) => {
+        return index === arr.findIndex(c2 => c.idCourse === c2.idCourse);
+      });
+    expect(onFilteredMock).toHaveBeenCalledWith(expectedOutput);
+    await user.clear(nameTextField); // clear the typed text
+  }
 });
 
-test("Credit Information is Correct", () => {
-  expect(true);
+test("Credit Filtering", async () => {
+  const user = setupUser();
+  const onFilteredMock = jest.fn();
+
+  const index = render(<CourseFiltering courseData={testCourseData} onFiltered={onFilteredMock}/>);
+  expect(index.baseElement).toMatchSnapshot();
+
+  const creditTab = screen.getByTestId("credit-tab");
+  await user.click(creditTab);
+
+  for (const course of testCourseData) {
+    await user.selectAutocomplete("Course Credits", course.credits.toString());
+    expect(onFilteredMock).toHaveBeenCalled();
+    const expectedOutput = testCourseData
+      .filter(c => c.credits === course.credits)
+      .filter((c, index, arr) => {
+        return index === arr.findIndex(c2 => c.idCourse === c2.idCourse);
+      });
+    expect(onFilteredMock).toHaveBeenCalledWith(expectedOutput);
+  }
 });

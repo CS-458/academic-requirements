733760f946cbc7142efb7c2225db946cba65afaa
ruudkas <kassidyruud@gmail.com>
2023-03-27 12:49:44 -0500

    minor change

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -1,5 +1,4 @@
 import AppBar from "@mui/material/AppBar";
-import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";

89dfff26a508708c3a6a41b7033f2a5cf52fdc27
Matthew Pomes <matthew.pomes@pm.me>
2023-04-19 13:13:45 -0500

    Fix SQL tests

--- __test__/service/sqlDb.test.ts
+++ __test__/service/sqlDb.test.ts
@@ -4,5 +4,7 @@ test("Load databases", async () => {
   const academic = await academicDb();
   expect(await academic.exists("major", "1=1")).toBe(true);
   const user = await userDb();
-  expect(await user.exists("user", "1=1")).toBe(true);
+  const counts = await user.get("select count(idUser), count(role) from user");
+  expect(counts["count(idUser)"]).toBeGreaterThanOrEqual(0);
+  expect(counts["count(role)"]).toBeGreaterThanOrEqual(0);
 });
